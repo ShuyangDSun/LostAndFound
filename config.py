4617947473c7d6ef1f3cea30ddcbbc93f4e52891
Daniel Sun <danielsun@v1041-wn-rt-b-186-172.campus-dynamic.uwaterloo.ca>
2022-05-15 22:37:39 -0400

    mysql environment variables

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'adfadsf341243#_A'
-    MYSQL_DATABASE_URI = os.environ.get('MYSQL_DATABASE_HOST') or 'mysql://root:my_sql123@localhost/lost_and_found'
-    DB_USER = os.environ.get('MYSQL_DATABASE_USER') or 'root'
-    DB_PASSWORD = os.environ.get('MYSQL_DATABASE_PASSWORD') or 'my_sql123'
-    DB_NAME = os.environ.get('MYSQL_DATABASE_DB') or 'lost_and_found'
+    MYSQL_DATABASE_URI = os.environ.get('MYSQL_HOST') or 'mysql://root:my_sql123@localhost/lost_and_found'
+    DB_USER = os.environ.get('MYSQL_USER') or 'root'
+    DB_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'my_sql123'
+    DB_NAME = os.environ.get('MYSQL_DB') or 'lost_and_found'
